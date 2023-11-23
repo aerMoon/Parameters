7f025743f7eb9d82eb3c65e58e243fab9f5ea630
egurnova <egurnova@COM409-T1>
2023-11-23 11:39:26 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        /*
+        создать метод, принимающий строку, и возвращающий массив слов*/
+        static void Main(string[] args)
+        {
+            //string str = Console.ReadLine();
+            //string[] arrayWords = GetWordsArray(str);
+            //foreach (string word in arrayWords) { Console.WriteLine(word); }
+            int[] Numbers = { -1, 2, 3, -1, -2, -3 };
+            int[] PositiveNumbers = NumbersArray(Numbers); 
+
+            foreach (int n in PositiveNumbers)
+            {
+                Console.WriteLine(n);
+            }
+        }
+        static string[] GetWordsArray(string s)
+        {
+            string[] words;
+            words = s.Split(' ');
+            return words;
+        }
+
+        //создать метод принимающий массив чисел,
+        //возвращающий массив только с полложительными значениями
+
+        static int[] NumbersArray(int[] array)
+        {
+            int count = 0;
+            foreach (int i in array)
+            {
+                if (i > 0)
+                {
+                    count++;
+                }
+
+            }
+            int[] result = new int[count];
+            int b = 0; //отдельный индекс для result
+            for (int i = 0; i < array.Length; i++) 
+            {
+                if (array[i] > 0)
+                {
+                    result[b] = array[i];
+                    b++;
+                }
+            }
+            return result;
+        }
+
+    }
+}
---
+++ Parameters/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Parameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 10;
+            int[,] mainArray;
+            PrintArray(ref size, 10, 99, out mainArray);
+            Console.WriteLine($"значение переменной size после работы метода {size}");
+
+            {
+                Console.WriteLine("первая строка массива:");
+                for (int i = 0; i < size; i++)
+                {
+                    Console.Write(mainArray[0, i] +", ");
+                }
+            }
+        }
+
+            static void PrintArray(ref int size, int min, int max, out int[,] array)
+            {
+                size = 5;
+                array = new int[size, size];
+                Random rnd = new Random();
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j< size; j++)
+                    {
+                        array[i, j] = rnd.Next(min, max+1);
+                        Console.Write(array[i, j]+", ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
